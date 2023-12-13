6bcf6ca54f4729066809c37134f63aa9fdb08311
Anthony <anthony@e-monsite.com>
2023-12-13 15:55:17 +0100

    add Quote Model and create Quote call

---
+++ lib/Api/WuroQuoteApi.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WuroClient\Api\Api;
+
+use Wuro\Api\Model\Quote;
+
+class WuroQuoteApi
+{
+    private $client;
+    private $config;
+
+    private bool $debug;
+
+    public function __construct(
+        ClientInterface $client,
+        Configuration $configuration,
+        bool $debug = false,
+    ) {
+        $this->client = $client;
+        $this->config = $configuration;
+        $this->debug = $debug;
+    }
+
+    public function createQuote(Quote $quote)
+    {
+        $uri = 'quote';
+
+        try {
+            $response = $this->client->send(
+                new Request(
+                    'POST',
+                    $this->config->getHost() . $uri,
+                    ApiFactory::getHeader(
+                        $this->config->getApiPublicKey(),
+                        $this->config->getApiSecretKey(),
+                        'POST',
+                        $uri
+                    ),
+                    json_encode(ApiFactory::getBody($quote))
+                )
+            );
+        } catch (RequestException $exception) {
+            throw new WuroApiException($exception->getMessage(), $exception->getCode());
+        }
+
+        return json_decode($response->getBody()->getContents());
+    }
+}
