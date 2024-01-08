a81cbe5c5d7969235c6bbb9d1ea62a544f1401c5
Anthony <anthony@e-monsite.com>
2024-01-08 16:28:42 +0100

    add wuro company api

---
+++ lib/Api/WuroCompanyApi.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WuroClient\Api\Api;
+
+use GuzzleHttp\ClientInterface;
+use WuroClient\Api\Configuration;
+use WuroClient\Api\Model\Company;
+
+class WuroCompanyApi
+{
+    private $client;
+    private $config;
+
+    private bool $debug;
+
+    public function __construct(
+        ClientInterface $client,
+        Configuration $configuration,
+        bool $debug = false,
+    ) {
+        $this->client = $client;
+        $this->config = $configuration;
+        $this->debug = $debug;
+    }
+
+    public function getCompany()
+    {
+        $uri = 'company';
+
+        try {
+            $response = $this->client->send(
+                new Request(
+                    'GET',
+                    $this->config->getHost() . $uri,
+                    ApiFactory::getHeader(
+                        $this->config->getApiPublicKey(),
+                        $this->config->getApiSecretKey(),
+                        'GET',
+                        $uri
+                    )
+                )
+            );
+        } catch (RequestException $exception) {
+            throw new WuroApiException($exception->getMessage(), $exception->getCode());
+        }
+
+        $content = json_decode($response->getBody()->getContents());
+
+        return new Company($content->company);
+    }
+}
---
+++ lib/Model/Company.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WuroClient\Api\Model;
+
+class Company extends AbstractModel
+{
+    public $_id;
+    public $name;
+    public $url;
+    public $emails;
+}
