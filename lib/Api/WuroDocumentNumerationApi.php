38425634f13c27aab4c02c7ca1211ff3bf0bf931
Anthony <anthony@e-monsite.com>
2024-05-22 11:20:15 +0200

    add document numerotation route

---
+++ lib/Api/WuroDocumentNumerationApi.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WuroClient\Api\Api;
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use WuroClient\Api\ApiFactory;
+use WuroClient\Api\Configuration;
+use GuzzleHttp\Psr7\Request;
+use WuroClient\Api\WuroApiException;
+
+class WuroDocumentNumerationApi
+{
+    private $client;
+    private $config;
+
+    private bool $debug;
+
+    public function __construct(
+        ClientInterface $client,
+        Configuration $configuration,
+        bool $debug = false,
+    ) {
+        $this->client = $client;
+        $this->config = $configuration;
+        $this->debug = $debug;
+    }
+
+    public function getDocumentNumber()
+    {
+        $uri = 'document-numeration';
+
+        try {
+            $response = $this->client->send(
+                new Request(
+                    'GET',
+                    $this->config->getHost() . $uri,
+                    ApiFactory::getHeader(
+                        $this->config->getApiPublicKey(),
+                        $this->config->getApiSecretKey(),
+                        'GET',
+                        $uri
+                    )
+                )
+            );
+        } catch (RequestException $exception) {
+            throw new WuroApiException($exception->getMessage(), $exception->getCode());
+        }
+
+        $content = json_decode($response->getBody()->getContents());
+
+        return $content->documentNumber;
+    }
+}
